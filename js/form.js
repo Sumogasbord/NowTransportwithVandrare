3b68f767e6538ca77dda84cb583037b6c31d4086
Sumögåsbord <sumogasbord@gmail.com>
2022-11-23 11:37:46 +0900

    v.0.0.4a
    編成番号が投稿できないのを修正。

--- js/form.js
+++ js/form.js
@@ -1,6 +1,6 @@
 function buttonClick() {
  var mstdnsvr = document.getElementById('mstdn_url');
- var dd = document.getElementById('date');
+ var dd = document.getElementById('date1');
  var trnsp = document.getElementById('transport');
  var brnd = document.getElementById('brand');
  var nbrg = document.getElementById('numbering')
@@ -14,7 +14,7 @@ function buttonClick() {
  var hs1 = document.getElementById('hash1');
  var hs2 = document.getElementById('hash2');
 
- var gene = mstdnsvr.value + "/share?text=" + dd.value + "%0a" + trnsp.value + "%20" + brnd.value + "%0a" + nbrg.value + "%20" + ln.value + "%0a" + ids.value + "%20" + "/" + "%20" + fr.value + "行" + "%0a" + gton.value + "%20" + "→" + "%20" + gtof.value + "%0a" + cmt.value + "%0a" + "%23" + hs1.value + "%20" + "%23" + hs2.value;
+ var gene = mstdnsvr.value + "/share?text=" + dd.value + "%0a" + trnsp.value + "%20" + brnd.value + "%0a" + nbrg.value + "%20" + ln.value + "%0a" + ids.value + "%20" + "/" + "%20" + fr.value + "行" + "%0a" + gton.value + "%20" + "→" + "%20" + gtof.value + "%0a" + vhcl.value + "%0a" + cmt.value + "%0a" + "%23" + hs1.value + "%20" + "%23" + hs2.value;
 
  let text = document.getElementById('output').innerText;
  document.getElementById('output').innerText = gene;
