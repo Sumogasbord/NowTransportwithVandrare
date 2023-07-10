dbc52fba2a9d7a131e51f82f19743ee382d135bf
sumogasbord <sumogasbord@gmail.com>
2023-07-10 10:14:57 +0900

    レスポンシブを強化
    あれだけ使い勝手悪そうな顔で使ってたのに今までせんかったん？

--- js/form_nextstops.js
+++ js/form_nextstops.js
@@ -5,18 +5,16 @@ function test() {
   var ids = document.getElementById('ids');
   var fr = document.getElementById('for');
   var next = document.getElementById('next');
-  var gton = document.getElementById('geton');
-  var gtof = document.getElementById('getoff');
   var cmt = document.getElementById('comment');
   var hs1 = document.getElementById('hash1');
   var hs2 = document.getElementById('hash2');
 
   if (hs1.value === '') {
-    var gene = trnsp.value + "\n" + nbrg.value + " " + ln.value + "\n" + ids.value + " " + "/" + " " + fr.value + "行" + "\n" + "［次は　" + next.value + "］" + "\n" + gton.value + " " + "→" + " " + gtof.value + "\n" + cmt.value;
+    var gene = trnsp.value + "\n" + nbrg.value + " " + ln.value + "\n" + ids.value + " " + "/" + " " + fr.value + "行" + "\n" + "［次は　" + next.value + "］" + "\n" + cmt.value;
   }else if (hs2.value === '') {
-    var gene = trnsp.value + "\n" + nbrg.value + " " + ln.value + "\n" + ids.value + " " + "/" + " " + fr.value + "行" + "\n" + "次は　" + next.value + "］" + "\n" + gton.value + " " + "→" + " " + gtof.value + "\n" + cmt.value + "\n" + "#" + hs1.value;
+    var gene = trnsp.value + "\n" + nbrg.value + " " + ln.value + "\n" + ids.value + " " + "/" + " " + fr.value + "行" + "\n" + "［次は　" + next.value + "］" + "\n" + cmt.value + "\n" + "#" + hs1.value;
   }else{
-    var gene = trnsp.value + "\n" + nbrg.value + " " + ln.value + "\n" + ids.value + " " + "/" + " " + fr.value + "行" + "\n" + "次は　" + next.value + "］" + "\n" + gton.value + " " + "→" + " " + gtof.value + "\n" + cmt.value + "\n" + "#" + hs1.value + " " + "#" + hs2.value;
+    var gene = trnsp.value + "\n" + nbrg.value + " " + ln.value + "\n" + ids.value + " " + "/" + " " + fr.value + "行" + "\n" + "［次は　" + next.value + "］" + "\n" + cmt.value + "\n" + "#" + hs1.value + " " + "#" + hs2.value;
   }
 
   let text = document.getElementById('output').innerText;
@@ -31,18 +29,16 @@ function buttonClick() {
   var ids = document.getElementById('ids');
   var fr = document.getElementById('for');
   var next = document.getElementById('next');
-  var gton = document.getElementById('geton');
-  var gtof = document.getElementById('getoff');
   var cmt = document.getElementById('comment');
   var hs1 = document.getElementById('hash1');
   var hs2 = document.getElementById('hash2');
 
  if (hs1.value === '') {
-  var gene_url = mstdnsvr.value + "/share?text=" + trnsp.value + "%0a" + nbrg.value + "%20" + ln.value + "%0a" + ids.value + "%20" + "/" + "%20" + fr.value + "行" + "%0a" + "［次は" + "%20" + next.value + "］" + "%0a" + gton.value + "%20" + "→" + "%20" + gtof.value + "%0a" + cmt.value;
+  var gene_url = mstdnsvr.value + "/share?text=" + trnsp.value + "%0a" + nbrg.value + "%20" + ln.value + "%0a" + ids.value + "%20" + "/" + "%20" + fr.value + "行" + "%0a" + "［次は" + "%20" + next.value + "］" + "%0a" + cmt.value;
 }else if (hs2.value === '') {
-  var gene_url = mstdnsvr.value + "/share?text=" + trnsp.value + "%0a" + nbrg.value + "%20" + ln.value + "%0a" + ids.value + "%20" + "/" + "%20" + fr.value + "行" + "%0a" + "［次は" + "%20" + next.value + "］" + "%0a" + gton.value + "%20" + "→" + "%20" + gtof.value + "%0a" + cmt.value + "%0a" + "%23" + hs1.value;
+  var gene_url = mstdnsvr.value + "/share?text=" + trnsp.value + "%0a" + nbrg.value + "%20" + ln.value + "%0a" + ids.value + "%20" + "/" + "%20" + fr.value + "行" + "%0a" + "［次は" + "%20" + next.value + "］" + "%0a" + cmt.value + "%0a" + "%23" + hs1.value;
 }else{
-  var gene_url = mstdnsvr.value + "/share?text=" + trnsp.value + "%0a" + nbrg.value + "%20" + ln.value + "%0a" + ids.value + "%20" + "/" + "%20" + fr.value + "行" + "%0a" + "［次は" + "%20" + next.value + "］" + "%0a" + gton.value + "%20" + "→" + "%20" + gtof.value + "%0a" + cmt.value + "%0a" + "%23" + hs1.value + "%20" + "%23" + hs2.value;
+  var gene_url = mstdnsvr.value + "/share?text=" + trnsp.value + "%0a" + nbrg.value + "%20" + ln.value + "%0a" + ids.value + "%20" + "/" + "%20" + fr.value + "行" + "%0a" + "［次は" + "%20" + next.value + "］" + "%0a" + cmt.value + "%0a" + "%23" + hs1.value + "%20" + "%23" + hs2.value;
 }
 
  const url = gene_url;
